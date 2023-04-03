a4a6e64af7f883877b1f73e34e4b67fec82b9a42
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-03 19:15:06 +0530

    Code updated

--- virtual_bank/src/components/Navbar.jsx
+++ virtual_bank/src/components/Navbar.jsx
@@ -8,8 +8,20 @@ const Navbar = () => {
     // <div>Navbar</div\>
     <nav className="w-full flex py-6 justify-between items-center navbar">
         <img src={logo}  alt="hoobank" className='w-[124px] h-[32px]'/>
-    </nav>
 
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+            {navLinks.map((nav,index) =>(
+                <li 
+                    key={nav.id}
+                    className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
+                <a href={`#${nav.id}`}>
+                    {nav.title}
+                </a>
+                </li>
+            ))}
+        </ul>
+    </nav>
+ 
   )
 }
 
