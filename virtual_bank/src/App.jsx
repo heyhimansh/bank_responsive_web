8e807f3d1be35d78ac0999a6f0f132ed594639af
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-03 18:52:02 +0530

    Code updated

--- virtual_bank/src/App.jsx
+++ virtual_bank/src/App.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import styles from "./style.js";
 
+import {Navbar,  Billing,CardDeal,Business,  Clients,CTA,Stats,Footer,Testimonials,Hero} from './components';
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>NAVBAR</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar/>
+
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-      HERO
+      <Hero/>
     </div>
   </div>
 
   <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-      STATS
-      BUSINESS
-      CTA
-      DEALS
-      FOOTER
-      
-    </div>
+      <Stats/>
+      <Business/>
+      <Billing/>
+      <CardDeal/>
+      <Testimonials/>
+      <Clients/>
+      <CTA />
+      <Footer/>
+
+      </div>
   </div>
 
   </div>
--- virtual_bank/src/components/Navbar.jsx
+++ virtual_bank/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import {close, logo ,menu} from '../assets';
+import { navLinks } from '../constants';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    // <div>Navbar</div\>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+        <img src={logo}  alt="hoobank" className='w-[124px] h-[32px]'/>
+    </nav>
+
   )
 }
 
