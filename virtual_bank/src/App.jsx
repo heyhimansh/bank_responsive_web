eb7ccdaf92c49cf25e366afd4e6f02a3d2e9f5ff
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-03 15:10:11 +0530

    Code update

--- virtual_bank/src/App.jsx
+++ virtual_bank/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import styles from "./style.js";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        NAVBAR
-      </div>
+      <div className={`${styles.boxWidth}`}>NAVBAR</div>
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+      HERO
+    </div>
+  </div>
+
+  <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+      STATS
+      BUSINESS
+      CTA
+      DEALS
+      FOOTER
+      
+    </div>
+  </div>
+
   </div>
 );
 
-export default App
+export default App;
