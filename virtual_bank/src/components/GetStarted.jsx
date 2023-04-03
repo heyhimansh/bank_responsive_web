52676e8c31c3751538eda09ac6e471f6ea386dfa
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-03 22:51:50 +0530

    Code updated

--- virtual_bank/src/components/GetStarted.jsx
+++ virtual_bank/src/components/GetStarted.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${styles.flexStart} flex-row`}>
+                <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                    <span className="text-gradient">Get</span>
+                    <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
+                </p>
+            </div>
+        </div>
+    </div>
+);
 
-export default GetStarted
+export default GetStarted;
