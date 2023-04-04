5e367ee7ac0357167a889822eb4a914914b21657
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-04 16:05:54 +0530

    Added

--- virtual_bank/src/components/Business.jsx
+++ virtual_bank/src/components/Business.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const FeatureCard = () => <div></div>;
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business , <br className="sm:block hidden" />
+          we'll handle the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature,index)=>(
+          <FeatureCard/>
+        ))}
 
-export default Business
+      </div>
+    </section>
+  );
+};
+
+export default Business;
