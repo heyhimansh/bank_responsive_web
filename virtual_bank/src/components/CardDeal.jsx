5282424ef47da7fec985db8dac47c0a76a86de91
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-04 18:59:40 +0530

    Added

--- virtual_bank/src/components/CardDeal.jsx
+++ virtual_bank/src/components/CardDeal.jsx
@@ -17,6 +17,11 @@ const CardDeal = () => (
       </p>
       <Button styles="mt-10" />
     </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+
+    </div>
   </section>
 );
 export default CardDeal;
