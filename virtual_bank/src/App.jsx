0e328bfae3463c1a1f5d72f17a5f8f9b5dd555c1
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-04 16:35:45 +0530

    Added

--- virtual_bank/src/App.jsx
+++ virtual_bank/src/App.jsx
@@ -1,37 +1,45 @@
 import React from "react";
 import styles from "./style.js";
 
-import {Navbar,  Billing,CardDeal,Business,  Clients,CTA,Stats,Footer,Testimonials,Hero} from './components';
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Navbar/>
-
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-      <Hero/>
-    </div>
-  </div>
-
-  <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-      <Stats/>
-      <Business/>
-      <Billing/>
-      <CardDeal/>
-      <Testimonials/>
-      <Clients/>
-      <CTA />
-      <Footer/>
-
+        <Hero />
       </div>
-  </div>
+    </div>
 
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 );
 
--- virtual_bank/src/components/Button.jsx
+++ virtual_bank/src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({styles}) => {
+const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>Get started</button>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
