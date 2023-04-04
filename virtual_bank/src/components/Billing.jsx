ec3c0a9b06f4d9991ee882f97bc9191606c1ef6f
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-04 18:27:49 +0530

    Added

--- virtual_bank/src/components/Billing.jsx
+++ virtual_bank/src/components/Billing.jsx
@@ -16,7 +16,7 @@ const Billing = () => (
       {/* gradient end */}
     </div>
 
-    <div className={layout.sectionInfo}>
+    <div className={`${layout.sectionInfo} ml-20 `} >
       <h2 className={styles.heading2}>
         Easily control your <br className="sm:block hidden" /> billing &
         invoicing
