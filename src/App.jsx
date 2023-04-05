8ae5cfcacf41c3bcd218f46d81f7852825a40db4
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-05 22:40:24 +0530

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style.js";
 // import LoadingBar from "react-top-loading-bar";
-import { Lines } from "react-preloaders";
+import {Sugar} from 'react-preloaders';
 
 import {
   Navbar,
@@ -22,7 +22,18 @@ const App = () => {
 
   return (
     <>
-      <Lines />
+      <Sugar background="radial-gradient(
+    64.18% 64.18% at 71.16% 35.69%,
+    #def9fa 0.89%,
+    #bef3f5 17.23%,
+    #9dedf0 42.04%,
+    #7de7eb 55.12%,
+    #5ce1e6 71.54%,
+    #33bbcf 100%
+  )" />
+  background-color: #0d6c9e;
+background-image: linear-gradient(0deg, #0d6c9e 0%, #19191a 100%);
+
       <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
