422e005ada0c27eb843c5d0bf5c0473a31be81c7
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-06 23:59:12 +0530

    Code updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,11 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`animate-ping py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+      data-te-ripple-init
+      data-te-ripple-color="light"
+      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
     >
-      Get started
+      Get Started
     </button>
   );
 };
