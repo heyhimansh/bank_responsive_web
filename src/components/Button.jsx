6778fcd7088f6dceb5c54af07f425c2a10d837d2
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-04-06 23:55:53 +0530

    Code updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+      className={`animate-ping py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     >
       Get started
     </button>
